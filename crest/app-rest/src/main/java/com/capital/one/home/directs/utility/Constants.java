872ef994072773d16d2fbfff3726054a24573b71
“isyedrahmath” <rahmath.syed@gmail.com>
2016-05-20 08:28:16 +0530

    app-rest projct deleted due to some configuration error

--- crest/app-rest/src/main/java/com/capital/one/home/directs/utility/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.capital.one.home.directs.utility;
-
-public class Constants {
-	
-	public static final String employeeDetailsExcelSheet = "E:\\MyWork\\Java\\support_work\\employee.xls";
-
-}
